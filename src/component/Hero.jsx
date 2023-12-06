0a5551910a43aea4381f46f8d940c4ab059dc061
zaryabdogar <zaryabdogar23@gmail.com>
2023-12-06 22:33:21 +0500

    commit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
- <section className="bg-white w-full py-12 font-sans pt-28">
-<div className="w-full flex justify-center flex-col 2xl:space-y-12 space-y-8">
+ <section className="bg-white w-full py-12 font-sans pt-24">
+<div className="w-full 2xl:pb-12 flex justify-center flex-col 2xl:space-y-12 space-y-8">
   <div className="flex w-full justify-center px-5">
-    <img src="/Hero.png" alt="" className='2xl:w-[680px] lg:w-[400px] w-[360px]' />
+    <img src="/Hero.png" alt="" className='2xl:w-[600px] 2xl:h-[450px]   h-auto lg:w-[340px] w-[360px]' />
   </div>
   <div className="text-center text-[#223568]">
     <p className="font-bold 2xl:text-[64px] text-[48px] leading-[54px] text-center">Innovate. Integrate. Inspire.</p>
